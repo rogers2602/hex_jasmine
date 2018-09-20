b5a74440fcb9f0d25197c74295fcdb190153b869
Tyler Nijmeh <tylernij@gmail.com>
2018-09-19 22:16:04 -0700

    hrtimer: fix implicit function call
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/time/hrtimer.c
+++ kernel/time/hrtimer.c
@@ -821,6 +821,16 @@ void hrtimers_resume(void)
 	clock_was_set_delayed();
 }
 
+static inline void timer_stats_account_hrtimer(struct hrtimer *timer)
+{
+#ifdef CONFIG_TIMER_STATS
+	if (likely(!timer_stats_active))
+		return;
+	timer_stats_update_stats(timer, timer->start_pid, timer->start_site,
+				 timer->function, timer->start_comm, 0);
+#endif
+}
+
 /*
  * Counterpart to lock_hrtimer_base above:
  */
